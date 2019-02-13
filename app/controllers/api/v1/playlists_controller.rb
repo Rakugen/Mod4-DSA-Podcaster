123b141c6ecfd999a9c8a8c6cbc03366ab0fb827
Rakugen <simonlee0306@gmail.com>
2019-02-13 12:01:50 -0500

    some crud on user

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::PlaylistsController < ApplicationController
+  def index
+    @playlists = Playlist.all
+    render json: @playlists
+  end
+
+  def show
+    @playlist = Playlist.find(params[:id])
+    render json: @playlist, status: :ok
+  end
+
+  def create
+    @playlist = Playlist.new(playlist_params)
+    @playlist.save
+    render json: @playlist, status: :created
+  end
+
+  def update
+    @playlist = Playlist.find(params[:id])
+    @playlist.update(playlist_params)
+    render json: @playlist, status: :accepted
+  end
+
+  private
+  def playlist_params
+    params.require(:playlist).permit(:name, :user_id, :podcast_id)
+  end
 end
